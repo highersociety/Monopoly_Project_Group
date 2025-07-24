ac1d9499339666750e25f4cdd757309c7a5080aa
trapboy01 <chriskantai95@gmail.com>
2025-07-24 17:31:11 +0300

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-
 import { useState, useEffect } from "react";
 
 function App({ position }) {
   const [currentSpace, setCurrentSpace] = useState(null);
-useEffect(() => {
+
+  useEffect(() => {
     fetch(`http://localhost:3000/spaces/${position}`)
       .then((res) => {
         if (!res.ok) throw new Error("Network response was not ok");
@@ -24,53 +24,4 @@ useEffect(() => {
 }
 
 export default App;
-🔍 Troubleshooting if you're still stuck on "Loading..."
-Is position defined and valid?
-Try logging it:
-
-js
-Copy
-Edit
-console.log("Current position:", position);
-Does http://localhost:3000/spaces/0 (or any position) work in your browser?
-You should get a JSON object like:
-
-json
-Copy
-Edit
-{
-  "id": 0,
-  "name": "GO",
-  "type": "Start",
-  "description": "Collect $200 as you pass."
-}
-Check your db.json is loaded properly by JSON Server
-Run:
-
-bash
-Copy
-Edit
-json-server --watch db.json
-You should see:
-
-bash
-Copy
-Edit
-Resources
-http://localhost:3000/spaces
-Is App.jsx being rendered at all?
-Check in Main.jsx (or index.js) that the component is used:
-
-jsx
-Copy
-Edit
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App position={0} />
-  </React.StrictMode>
-);
 
