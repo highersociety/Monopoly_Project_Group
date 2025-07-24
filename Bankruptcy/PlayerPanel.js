fc2d91fd156082a9aa6acc9ddefeddcbaea79d86
trapboy01 <chriskantai95@gmail.com>
2025-07-25 02:26:13 +0300

    player panel

--- Bankruptcy/PlayerPanel.js
+++ Bankruptcy/PlayerPanel.js
@@ -1,18 +1,20 @@
-import { usePlayerContext } from "./PlayerContext";
+import { usePlayers } from "./PlayerContext";
 
 const PlayerPanel = () => {
-  const { players } = usePlayerContext();
+  const { players } = usePlayers();
 
   return (
     <div className="player-panel">
-      {players.map(player => (
+      {players.map((player) => (
         <div
           key={player.id}
           className={`player-card ${player.isBankrupt ? "bankrupt" : ""}`}
         >
           <h3>{player.name}</h3>
-          <p>Balance: ${player.balance}</p>
-          {player.isBankrupt && <span className="bankrupt-label">Bankrupt</span>}
+          <p>Balance: ${player.money}</p>
+          {player.isBankrupt && (
+            <span className="bankrupt-label">💥 Bankrupt</span>
+          )}
         </div>
       ))}
     </div>
@@ -20,4 +22,3 @@ const PlayerPanel = () => {
 };
 
 export default PlayerPanel;
-
