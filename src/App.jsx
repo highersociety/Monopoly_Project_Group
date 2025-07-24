c734bcef2c379c2715a6da3ecbbface42b665a22
trapboy01 <chriskantai95@gmail.com>
2025-07-24 17:48:59 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
 function App() {
-  return <h1>Hello from App</h1>;
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src={viteLogo} />
+      </a>
+      <a href="https://react.dev" target="_blank">
+        <img src={reactLogo} />
+      </a>
+      <h1>Vite + React</h1>
+      <div>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>Edit <code>src/App.jsx</code> and save to test HMR</p>
+      </div>
+    </div>
+  )
 }
 
-export default App;
+export default App
