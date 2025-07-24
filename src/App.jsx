fb4a8627ed0766f5363406737745c5ae39a5ea9d
trapboy01 <chriskantai95@gmail.com>
2025-07-24 17:46:54 +0300

    index

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,5 @@
-import { useState, useEffect } from "react";
-
-function App({ position }) {
-  const [currentSpace, setCurrentSpace] = useState(null);
-
-  useEffect(() => {
-    fetch(`http://localhost:3000/spaces/${position}`)
-      .then((res) => {
-        if (!res.ok) throw new Error("Network response was not ok");
-        return res.json();
-      })
-      .then((data) => setCurrentSpace(data))
-      .catch((err) => console.error("Error fetching space:", err));
-  }, [position]);
-
-  if (!currentSpace) return <p>Loading space info...</p>;
-
-  return (
-    <div>
-      <h1>{currentSpace.name}</h1>
-      <p>{currentSpace.description}</p>
-    </div>
-  );
+function App() {
+  return <h1>Hello from App</h1>;
 }
 
 export default App;
-
--- src/Main.jsx
+++ src/Main.jsx
@@ -1,5 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App position={0} /> {/* Or any other starting position */}
+    <App />
   </React.StrictMode>
 );
+import './style.css';
